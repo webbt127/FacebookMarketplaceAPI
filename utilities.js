aad8fd2567e0691554fa9061212528a98be926f7
webbt127 <80007652+webbt127@users.noreply.github.com>
2024-05-19 10:14:54 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -31,7 +31,7 @@ export async function searchFacebook(params){
     });
     url += `&exact=false`;
     console.log(url);
-    const browser = await puppeteer.launch({ headless: runHeadless, executablePath: '/usr/bin/chromium-browser' });
+    const browser = await puppeteer.launch({ headless: runHeadless, executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url);
     const closeButtonSelector = 'div[aria-label="Close"][role="button"]';
