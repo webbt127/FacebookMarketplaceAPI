b4ef1a32a80e15dc301b1a394d87d4cd8cc19b21
webbt127 <80007652+webbt127@users.noreply.github.com>
2024-05-20 12:11:23 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -20,7 +20,7 @@ function getRadiusBox(radius) {
 }
 
 export async function searchFacebook(params){
-    var runHeadless = false;
+    var runHeadless = true;
     if (!params.posts) {params.posts = 20};
     console.log(`${params.posts} posts requested for ${params.location}`);
     var url = 'https://www.facebook.com/marketplace/search/?';
@@ -36,18 +36,11 @@ export async function searchFacebook(params){
     //const browser = await puppeteer.launch({ headless: runHeadless});
     const page = await browser.newPage();
     puppeteer.use(pluginStealth())
-    // await page.setExtraHTTPHeaders({ 
-	// 	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-	// 	'upgrade-insecure-requests': '1', 
-	// 	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
-	// 	'accept-encoding': 'gzip, deflate, br', 
-	// 	'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
-	// }); 
     await page.goto(url);
     await page.screenshot({ path: 'image.png', fullPage: true });      
     const closeButtonSelector = 'div[aria-label="Close"][role="button"]';
-    //await page.waitForSelector(closeButtonSelector);
-    //await page.click(closeButtonSelector);
+    await page.waitForSelector(closeButtonSelector);
+    await page.click(closeButtonSelector);
     page.setViewport({width: 800, height: 1000});
     
     if (params.location) {
