dc3e3f0aecc2eb60628e994382003f4d833034be
webbt127 <80007652+webbt127@users.noreply.github.com>
2024-05-20 11:06:40 -0500

    .

--- utilities.js
+++ utilities.js
@@ -1,4 +1,5 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import pluginStealth from "puppeteer-extra-plugin-stealth";
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -34,13 +35,14 @@ export async function searchFacebook(params){
     const browser = await puppeteer.launch({ headless: runHeadless, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     //const browser = await puppeteer.launch({ headless: runHeadless});
     const page = await browser.newPage();
-    await page.setExtraHTTPHeaders({ 
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-		'upgrade-insecure-requests': '1', 
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
-		'accept-encoding': 'gzip, deflate, br', 
-		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
-	}); 
+    puppeteer.use(pluginStealth())
+    // await page.setExtraHTTPHeaders({ 
+	// 	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+	// 	'upgrade-insecure-requests': '1', 
+	// 	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+	// 	'accept-encoding': 'gzip, deflate, br', 
+	// 	'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+	// }); 
     await page.goto(url);
     await page.screenshot({ path: 'image.png', fullPage: true });      
     const closeButtonSelector = 'div[aria-label="Close"][role="button"]';
