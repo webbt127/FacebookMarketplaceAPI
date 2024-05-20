eee585ffe105eb96d6e152e740cb8ba7c71d528b
webbt127 <80007652+webbt127@users.noreply.github.com>
2024-05-20 00:14:27 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -73,8 +73,8 @@ export async function searchFacebook(params){
         await page.keyboard.press('ArrowDown');
         await page.keyboard.press('PageDown');
     };
-    const marketplaceGrid = await page.$('body > div > div > div > div > div:nth-of-type(3) > div > div > div > div > div:nth-of-type(2)');
-    console.log(marketplaceGrid)
+    const marketplaceGrid = await page.$$('body > div > div > div > div'); // > div:nth-of-type(3) > div > div > div > div > div:nth-of-type(2)');
+    console.log(marketplaceGrid.length)
     const posts = await marketplaceGrid.$$('a');
     let postArray = [];
 
