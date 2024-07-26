055f703720dfdddceec6e01b650e088e3c6d5be4
webbt127 <80007652+webbt127@users.noreply.github.com>
2024-07-26 11:30:43 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
 import pluginStealth from "puppeteer-extra-plugin-stealth";
 
 function wait(ms) {
@@ -35,10 +35,13 @@ export async function searchFacebook(params){
     //const browser = await puppeteer.launch({ headless: runHeadless, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     //const browser = await puppeteer.launch({ headless: runHeadless});
     //const page = await browser.newPage();
-    const browser = await puppeteer.connect({browserWSEndpoint: 'ws://localhost:3000?token=6R0W53R135510'});
-    const page = await browser.newPage();
-    puppeteer.use(pluginStealth())
-    await page.goto(url);
+    await puppeteer
+    .launch()
+    .then(async (browser) => {
+      const page = await browser.newPage();
+      await page.goto(url);
+    })
+    //await page.goto(url);
     await page.screenshot({ path: 'image.png', fullPage: true });      
     const closeButtonSelector = 'div[aria-label="Close"][role="button"]';
     await page.waitForSelector(closeButtonSelector);
